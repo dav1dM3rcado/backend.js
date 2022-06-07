0e032e8301a2498536957e097dc88ee38e7e8c1c
dav1dM3rcado <91168753+dav1dM3rcado@users.noreply.github.com>
2022-06-07 00:38:45 -0300

    agregamos los middlewares

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-
+import morgan from "morgan";
+import cors from "cors";
 //creo una instacia de express
 const app = express();
 
@@ -8,4 +9,20 @@ app.set("port", process.env.PORT || 4000);
 
 app.listen(app.get("port"), () => {
   console.log("estoy en el puerto" + app.get("port"));
+});
+
+//midlewares
+app.use(morgan("dev")); // da informacion en la terminal
+app.use(cors()); //permite peticiones remotas
+//permite recibir objetos en formato json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// ruta de prueba
+app.get("/products", (req, res) => {
+  // lo que quiero que pase cuando se ejecute esta consulta
+  res.send("hola desde el backend");
+});
+app.get("/", (req, res) => {
+  // lo que quiero que pase cuando se ejecute esta consulta
+  res.send("hola desde el backend");
 });

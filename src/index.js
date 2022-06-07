3aed7f71f255574de8b4a5e073e2b234555f4d5c
dav1dM3rcado <91168753+dav1dM3rcado@users.noreply.github.com>
2022-06-06 22:46:47 -0300

    agregamos mas configuracion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import express from "express";
-console.log("prueba de backend");
 
 //creo una instacia de express
 const app = express();
+
+// creao un puerto
+app.set("port", process.env.PORT || 4000);
+
+app.listen(app.get("port"), () => {
+  console.log("estoy en el puerto" + app.get("port"));
+});
